843241b96ae0a94ec009c1c2057d40d80e088440
Nico Basile <nbasile@usc.edu>
2020-07-09 23:36:32 +0000

    Added edit profile functionality

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,11 +13,30 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile( profile_params )
     if @profile.save
       flash[:success] = "Profile updated!"
-      redirect_to user_path( params[:user_id] )
+      redirect_to user_path(id: params[:user_id] )
     else
       render action: :new
     end
   end
+  
+  # GET to /users/:user_id/profile/edit
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  
+  # PUT/PATCH to /users/:user_id/profile
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes( profile_params )
+      flash[:success] = "Profile updated!"
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :profession, :phone_number, :contact_email, :description)
